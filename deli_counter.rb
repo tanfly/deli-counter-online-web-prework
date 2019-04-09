24b0a889e17aa5fa543ce67f5496945698a97dfd
Stella McEwen <mcewen.stella@gmail.com>
2019-04-09 22:48:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else 
-    array.each.with_index do |name, index|
+    array.each_with_index do |name, index|
       lineup.push ("#{index}. #{name}")
     end
     puts "The line is currently: #{lineup.join("")}"
