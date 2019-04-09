54bbea0a3caecaf2c63b668565c5235af419e17e
Stella McEwen <mcewen.stella@gmail.com>
2019-04-09 22:40:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,6 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else 
-    array.each.with_index(1) do |
+    array.each.with_index(1) do |name, index|
+      
   
