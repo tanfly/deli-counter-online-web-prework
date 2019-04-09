c5b109c2b01ac00ee64e05e74ee9d3de977128b8
Stella McEwen <mcewen.stella@gmail.com>
2019-04-09 21:48:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,2 @@
 def line(array)
-  if array.length >= 1 
-    line = []
-    counter = 1 
-    array.each do |name|
-      line.push("#{name} is number #{counter} in line.")
-      counter += 1 
-  end
+  if array.empty?
