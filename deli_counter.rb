f3af8807b557a5b0fb94454ba7faa6546f49e021
Stella McEwen <mcewen.stella@gmail.com>
2019-04-09 22:53:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(array)
     message="The line is currently:"
     
     array.each_with_index do |name, index|
-      message += "#{index + 1}. #{name} "
+      message += " #{index + 1}. #{name}"
     end
     puts "#{message}"
   end
