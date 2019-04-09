73f9ddc7968333c029ee24045f88d538790d29d6
Stella McEwen <mcewen.stella@gmail.com>
2019-04-09 21:27:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(array)
   line = []
   counter = 0 
   while counter == 0
+  puts "The line is currently empty."
   array.each do |lineup|
     line.push("#{lineup} is number #{counter} in line.")
     
