fb80d90ac0fa1bc2186f56ef6e4d54a1492b6b7b
Stella McEwen <mcewen.stella@gmail.com>
2019-04-09 21:29:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,6 @@ def line(array)
   while counter == 0
   puts "The line is currently empty."
   break if counter >= 1 
-end 
   array.each do |lineup|
     line.push("#{lineup} is number #{counter} in line.")
   counter += 1 
